5e8da84c66606beba5daf9bda10ad346e40a4474
Kyle Saunders <101525834+kylersaunders@users.noreply.github.com>
2022-12-10 15:58:30 -0500

    update backend with middleware routes and db structure

---
+++ server/routes/pins.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+//require controllers and db models
+const controllers = require('../controllers/controller.js');
+
+router.post('/', controllers.createPin, (req, res, next) => {
+  // console.log('REQ', req.body, res);
+  res.send(res.locals.response);
+});
+
+router.put('/', controllers.updatePin, (req, res, next) => {
+  res.send(res.locals.response);
+});
+
+router.delete('/', controllers.deletePin, (req, res, next) => {
+  res.send('Pin successfully deleted');
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,109 +1,56 @@
-var express = require('express'); // Express web server framework
-var app = express();
-// var request = require('request'); // "Request" library
-// var cors = require('cors');
-// var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
-// const Buffer = require('buffer').Buffer;
+const express = require('express');
+const app = express();
 const path = require('path');
+var cookieParser = require('cookie-parser');
 
 //mostly for 3rd party api calls from backend middleware
 app.use(cookieParser()).use(express.json());
 
-const db = require('./models/schmaapMapModel');
+//define routers
+const userRouter = require('./routes/user');
+const pinRouter = require('./routes/pins');
+
+//define controller and db models
 const controllers = require('./controllers/controller.js');
-// const cs = require('./clientSecret');
 
-// const apiRouter = require('./routes/api');
-// app.use('/api', apiRouter);
+// app.get('/signup', (req, res, err) => {
+//   // res.sendFile(path.resolve(__dirname, '../client/signup.html'));
+// });
 
-app.get(
-  '/user',
-  controllers.checkAuth,
-  controllers.getUser,
-  (req, res, err) => {
-    return res.json(res.locals.response);
-  }
-);
+// app.get('/login', (req, res, err) => {
+//   // res.sendFile(path.resolve(__dirname, '../client/login.html'));
+// });
 
-// const userAPI = express.Route();
-// app.use('/user', userAPI);
-
-app.post('/user', controllers.createUser, (req, res, err) => {
-  // console.log('REQ', req.body, res);
-  res.send(res.locals.response);
+app.post('/login', controllers.login, (req, res, err) => {
+  console.log('SUCCESS!');
+  res.send();
 });
 
-// app.put('/user');
-
-app.delete('/user', controllers.deleteUser, (req, res, err) => {
-  res.send('user deleted');
+app.post('/signup', controllers.login, (req, res, err) => {
+  console.log('SUCCESS!');
+  res.send();
 });
 
-/////////
-
-app.get('/pin');
-
-app.post('/pin');
-
-// app.put('/pin')
-
-app.delete('/pin');
-
-//////////
-
-// app.get('/tag');
-
-// app.post('/tag');
-
-// // app.put('/tag')
-
-// app.delete('/tag');
-
-/////////
-
-app.get('/signup');
-
-app.post('/signup');
-
-app.get('/login');
-
-app.post('/login');
-
-//////
-
-// app.get('/login', (req, res) => {});
-
-// app.post(
-//   '/login',
-//   sqlController.authorize,
-//   sqlController.getUserData,
-//   (req, res, err) => {
-//     return res.json(res.local.response);
-//   }
-// );
-
-// app.get(
-//   '/getUserData',
-//   sqlController.checkAuth,
-//   sqlController.getUserData,
-//   (req, res, err) => {
-//     return res.json(res.locals.response);
-//   }
-// );
-
-app.use((err, req, res, next) => {
-  console.log('ERROR: ', err);
-});
+app.use('/user', userRouter);
+app.use('/pin', pinRouter);
 
 if (process.env.NODE_ENV === 'production') {
   // statically serve everything in the build folder on the route '/build'
   app.use('/build', express.static(path.join(__dirname, '../build')));
   // serve index.html on the route '/'
-  app.get('/', (req, res) => {
+  app.get('/', controllers.checkAuth, (req, res) => {
     return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
   });
 }
+
+//delete when we start using webpack to debug fullstack
+app.get('/', (req, res, err) => {
+  res.sendFile(path.resolve(__dirname, '../index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.log('ERROR: ', err);
+});
 
 console.log('Listening on 3000');
 app.listen(3000);
