fd00d3895d2f0182207565cede59f7a87482ad25
Sami Messai <messai.sami@gmail.com>
2022-12-10 14:07:12 -0600

    working map with markers and popups

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
-
+import Map from './mainpage-components/Map';
 
 const App = () => {
-  return <div>Hello From the App</div>;
+  return (
+    <div>
+      <Map />
+    </div>
+  );
 };
 
 export default App;
